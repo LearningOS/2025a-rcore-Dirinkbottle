efa7a63cbdf97aa203c626b4021bdde10c8da349
dirinkbottle <2909128143@qq.com>
2025-09-26 22:43:42 +0800

    ch7

--- os/src/main.rs
+++ os/src/main.rs
@@ -67,7 +67,7 @@ fn clear_bss() {
 pub fn rust_main() -> ! {
     clear_bss();
     println!("[kernel] Hello, world!");
-    println!(".");
+    println!("[kernel] Github Push Test!");
     logging::init();
     mm::init();
     mm::remap_test();
