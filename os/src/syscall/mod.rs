920948dd11d21975f3a6c5d6c47153251ef32a54
dirinkbottle <2909128143@qq.com>
2025-09-26 13:16:16 +0800

    Update mod.rs

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -9,6 +9,9 @@
 //! For clarity, each single syscall is implemented as its own function, named
 //! `sys_` then the name of the syscall. You can find functions like this in
 //! submodules, and you should also implement syscalls this way.
+//! 
+//! 
+///syscall
 pub const  MAX_SYSCALLID:usize=1000;
 /// write syscall
 const SYSCALL_WRITE: usize = 64;
